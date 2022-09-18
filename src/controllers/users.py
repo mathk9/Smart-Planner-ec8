73c14d131097a96015aef00420bbf47a19c0c9ce
Matheus de Novais Souza <108086621+MatheusNovaisOV@users.noreply.github.com>
2022-09-18 13:28:51 -0300

    V1.0 - User API

---
+++ src/controllers/users.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_restx import Api, Resource
+
+from src.server.instance import server
+from src.models.users import user
+
+app, api = server.app, server.api
+
+users_db = [
+    {'id': 0, 'name': 'Matheus'},
+    {'id': 1, 'name': 'Ysabela'},
+    {'id': 2, 'name': 'Caio'},
+    {'id': 3, 'name': 'Giovana'},
+]
+
+ITEM_NOT_FOUND = "User not found."
+
+@api.route('/v1.0/user/<int:id>')
+class User(Resource):
+    @api.doc('Get an User')
+    def get(self, id):
+        for i in range(len(users_db)):
+            if users_db[i]['id'] == id:
+                return users_db[i]
+        return {'message': ITEM_NOT_FOUND}, 404
+
+    @api.doc('Delete an User')
+    def delete(self, id):
+        for i in range(len(users_db)):
+            if users_db[i]['id'] == id:
+                del users_db[i]
+                return '', 204
+        return {'message': ITEM_NOT_FOUND}, 404
+
+@api.route('/v1.0/users')
+class Userlist(Resource):
+    @api.doc('Get all Users')
+    @api.marshal_list_with(user)
+    def get(self):
+        return users_db
+
+    @api.expect(user, validate=True)
+    def post(self):
+        response = api.payload
+        users_db.append(response)
+        return response, 200        
---
+++ src/models/users.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from flask_restx import fields
+
+from src.server.instance import server
+
+user = server.api.model('User', {
+    'id': fields.String(description='O ID do registro.'),
+    'name': fields.String(required=True, min_Length=1, max_Length=200,description='O Nome do registro.')
+})
